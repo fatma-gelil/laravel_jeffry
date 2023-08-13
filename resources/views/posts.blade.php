114d17d3364400b8153cda5e5b2293c386be9589
fatma Gelil <gelilfatma79@gmail.com>
2023-08-13 15:44:06 +0300

    jeffry/laravel

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="">
+    <head>
+        <title>Laravel Blog</title>
+        <!-- Styles -->
+        <link rel="stylesheet" href="resources/css/app.css">
+
+    </head>
+    <body>
+    <h1>welcome</h1>
+    <article>
+        <h2> <a href="resources/posts/my-first-post">post 1 </a> </h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium alias aliquam aliquid autem deleniti enim
+            , excepturi fugiat illum ipsa mollitia officia placeat porro quidem quod soluta tenetur unde, ut vel.</p>
+    </article>
+    <article>
+        <h2> <a href="resources/posts/my-second-post">post 2 </a> </h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium alias aliquam aliquid autem deleniti enim
+            , excepturi fugiat illum ipsa mollitia officia placeat porro quidem quod soluta tenetur unde, ut vel.</p>
+    </article>
+    <article>
+        <h2> <a href="resources/posts/my-third-post">post 3 </a> </h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium alias aliquam aliquid autem deleniti enim
+            , excepturi fugiat illum ipsa mollitia officia placeat porro quidem quod soluta tenetur unde, ut vel.</p>
+    </article>
+
+    </body>
+</html>
