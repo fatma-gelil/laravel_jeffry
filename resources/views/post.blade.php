a806a6bf655aa0d74be458d9338e7d3712e0fb9c
fatma Gelil <gelilfatma79@gmail.com>
2023-08-13 15:43:12 +0300

    jeffry/laravel

---
+++ resources/views/post.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+	<title>Laravel Blog</title>
+	<!-- Styles -->
+
+
+</head>
+<body>
+<h1>welcome</h1>
+<article>
+	<h2> <a href="/post">post</a> </h2>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium alias aliquam aliquid autem deleniti enim
+		, excepturi fugiat illum ipsa mollitia officia placeat porro quidem quod soluta tenetur unde, ut vel.</p>
+</article>
+<a href="/">home</a>
+</body>
+</html>
